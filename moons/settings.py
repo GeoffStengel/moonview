af7442893eaae6616f01259fdea42979b842a8bf
unknown <geoffsglass@hotmail.com>
2024-09-07 10:07:52 -0800

    Added Auto Deploy From GitHub push to Heroku.  Site still not showing up.  Seems to be between Static Configuration in settings.py, or Procfile not set correctly

--- moons/settings.py
+++ moons/settings.py
@@ -139,7 +139,9 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #WE ADDED THIS
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
 
 django_heroku.settings(locals())
